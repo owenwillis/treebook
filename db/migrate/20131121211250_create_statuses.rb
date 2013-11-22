122e9518f74548dc422fb9df8a172ad70f2bf5e0
Owen Willis <owenwillis@gmail.com>
2013-11-22 13:48:42 -0500

    added bootstrap, and applied layout to the page

--- db/migrate/20131121211250_create_statuses.rb
+++ db/migrate/20131121211250_create_statuses.rb
@@ -3,7 +3,7 @@ class CreateStatuses < ActiveRecord::Migration
     create_table :statuses do |t|
       t.string :name
       t.string :content
-      t.string :text
+     
 
       t.timestamps
     end
